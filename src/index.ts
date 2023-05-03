b4d6e5b5fd7cb14b53696bb5860b43cf913efc66
Simon Lepel <simbo@simbo.de>
2023-05-03 09:18:17 +0200

    fix linting issues

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { encodeHTML } from 'entities';
 
 export function encodeEmojis(str: string): string {
   return str.replaceAll(
-    /(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])+/g,
+    /(\u00A9|\u00AE|[\u2000-\u3300]|\uD83C[\uD000-\uDFFF]|\uD83D[\uD000-\uDFFF]|\uD83E[\uD000-\uDFFF])+/g,
     match => encodeHTML(match)
   );
 }
